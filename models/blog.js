13f58aa7e53153523c6b29164b787343a4fdb68a
LinearLaw <17817363770@163.com>
2018-05-15 16:08:36 +0800

    update blog schema

---
+++ models/blog.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const db = require("./db.js");
+
+const blogSchema = new mongoose.Schema({
+    blogId:         {   type:String },    //博客id
+    blogAuthorId:   {   type:String },    //作者的userId
+    blogTitle:      {   type:String },    //博文标题
+    content:        {   type:String },    //博文内容
+    createTime:     {   type:Number },    //创建时间
+    updateTime:     {   type:Number }     //更新时间
+})
+
+const Blog = mongoose.model("Blog",blogSchema);
+
+module.exports = Blog;
