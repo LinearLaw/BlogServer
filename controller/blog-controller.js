f37a4092901dd9c318a18950ee615657aa912d74
LinearLaw <17817363770@163.com>
2018-05-16 17:41:15 +0800

    add short content

--- controller/blog-controller.js
+++ controller/blog-controller.js
@@ -1,6 +1,9 @@
+const formidable = require('formidable');
+const cheerio = require("cheerio")
+
 const Blog = require("../models/blog.js");
 const color = require('colors-cli');
-const formidable = require('formidable');
+
 
 exports.reportBlog = (req,res)=>{
     console.log(color.green("reportBlog收到请求"));
@@ -20,14 +23,19 @@ exports.reportBlog = (req,res)=>{
             })
             return;
         }
-        
+        var $ = cheerio.load(fields.blogContent);
+        var innerText = $.text();
+        if(innerText.length>30){
+            innerText = innerText.slice(0,30) + "...";
+        }
+        var shortContent = innerText;
         //fields为post body的内容
         let reqObj = {
             blogId:         CONFIG.idCreate.appleSignal(),    //博客id
             blogAuthorId:   "admin",    //作者的userId
             blogTitle:      fields.blogTitle,    //博文标题
             blogContent:    fields.blogContent,    //博文内容
-            blogShortContent:    fields.blogContent,    //博文内容
+            blogShortContent:    shortContent,    //博文内容
             createTime:     new Date().getTime(),    //创建时间
             updateTime:     new Date().getTime()     //更新时间
         }
@@ -46,7 +54,6 @@ exports.reportBlog = (req,res)=>{
  */
 exports.getBlogList = (req,res)=>{
     console.log(color.green("getBlogList收到请求"));
-    console.log(req.query);
     if(!req.query.blogAuthorId){
         res.send({
             status:2,
@@ -58,11 +65,22 @@ exports.getBlogList = (req,res)=>{
      * params : userId
      */
     Blog.find({"blogAuthorId":req.query.blogAuthorId},(err,result)=>{
-
+        //make response faster, do map for avoid send detail content
+        var resArr = [];
+        result.map(function(item,index){
+            resArr.push({
+                blogId:         item.blogId,    //博客id
+                blogAuthorId:   item.blogAuthorId,    //作者的userId
+                blogTitle:      item.blogTitle,    //博文标题
+                blogShortContent:    item.blogShortContent,    //博文略缩内容
+                createTime:     item.createTime,    //创建时间
+                updateTime:     item.updateTime     //更新时间
+            })
+        })
         res.send({
             status:1,
             content:"success",
-            data:result
+            data:resArr
         })
     })
 }
@@ -91,6 +109,9 @@ exports.getBlogDetail = (req,res)=>{
     })
 }
 
+/*
+ * @desc 删除某一条博客
+ */
 exports.deleteBlog = (req,res)=>{
     console.log(color.green("getBlogDetail收到请求"));
     if(!req.query.blogId){
