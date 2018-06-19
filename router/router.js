5ebba87a884b440520eca8f261a914f2baadf63f
LinearLaw <17817363770@163.com>
2018-06-19 15:55:03 +0800

    update

--- router/router.js
+++ router/router.js
@@ -7,22 +7,22 @@ const router = express.Router();
 
 
 /**
- * @desc blog-service 
+ * @desc blog-service
  */
 //user-service
 
 
 //blog-service
-router.post("/reportBlog", blogController.reportBlog);
-router.get("/getBlogList", blogController.getBlogList);
-router.get("/getBlogDetail", blogController.getBlogDetail);
-router.get("/deleteBlog", blogController.deleteBlog);
+router.post("/reportBlog"   , blogController.reportBlog);
+router.get("/getBlogList"   , blogController.getBlogList);
+router.get("/getBlogDetail" , blogController.getBlogDetail);
+router.get("/deleteBlog"    , blogController.deleteBlog);
 
 
 //Comment service
-router.post("/reportComment", commentController.reportComment);
-router.post("/deleteComment", commentController.deleteComment);
-router.get("/getCommentList", commentController.getCommentList);
+router.post("/reportComment"  , commentController.reportComment);
+router.post("/deleteComment"  , commentController.deleteComment);
+router.get("/getCommentList"  , commentController.getCommentList);
 
 
 module.exports = router;
